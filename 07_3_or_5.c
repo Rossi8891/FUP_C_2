619bc44d0770a203083d38102f5f4928e13e3c2a
Rossi8891 <enzogazirerossi@gmail.com>
2026-04-08 16:28:06 -0300

    new commit

---
+++ 07_3_or_5.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+    int a;
+    scanf("%d", &a);
+    
+    if (a == 3 || a == 5){
+        printf("SIM\n");
+    }
+    else{
+        printf("NAO\n");
+    }
+}
